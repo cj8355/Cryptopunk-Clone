cd016739280c24627d8c6330354c0727b89b210e
Charles Callender <charlescallender.cc@gmail.com>
2022-05-05 21:29:24 -0700

    Added headers to fix CORS

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Main from './components/Main';
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
-  const [selectedPunk, setSelectedPunk] = useState(0);
+  const [selectedPunk, setSelectedPunk] = useState(0);s
 
   useEffect(() => {
     const getMyNfts = async () => {
