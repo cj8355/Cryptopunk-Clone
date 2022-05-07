eb31d953156af91ba30ce8b24e95d769694f6fbf
Charles Callender <charlescallender.cc@gmail.com>
2022-05-07 12:18:57 -0700

    Updated proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/assets?asset_contract_address=0xB6183ec9664feD2901B2D17c30ac324Fe8e63192&order_direction=asc&offset=0&limit=20',
-    createProxyMiddleware({
+    
+    createProxyMiddleware('/assets?asset_contract_address=0xB6183ec9664feD2901B2D17c30ac324Fe8e63192&order_direction=asc&offset=0&limit=20', {
       target: 'https://testnets-api.opensea.io',
       changeOrigin: true,
     })
