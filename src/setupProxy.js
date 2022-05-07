5f6cacc60f0ac921fb35d89672f39b2c5edfb6a6
Charles Callender <charlescallender.cc@gmail.com>
2022-05-06 18:01:56 -0700

    Added proxy to fix cors

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,9 @@
-const proxy = require('http-proxy-middleware')
+const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = function(app) {
     app.use(
-        proxy("/assets?asset_contract_address=0xB6183ec9664feD2901B2D17c30ac324Fe8e63192&order_direction=asc&offset=0&limit=20", {
+        "/assets?asset_contract_address=0xB6183ec9664feD2901B2D17c30ac324Fe8e63192&order_direction=asc&offset=0&limit=20",
+        createProxyMiddleware({
             target: "https://testnets-api.opensea.io",
             changeOrigin: true
         })
